23f23cd97dc87a4eb9d9fa547dd9698e681e1f3b
Kevin Jones <kevin@rocksolidknowledge.com>
2017-02-14 07:07:47 +0000

    Bootstrap layout code including NavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
-import { NgModule }      from "@angular/core"
-import { BrowserModule } from "@angular/platform-browser"
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic"
-import { AppComponent } from "./app.component"
-import { RouterModule, PreloadAllModules } from "@angular/router";
+import {NgModule}      from "@angular/core"
+import {NgbModule}     from "@ng-bootstrap/ng-bootstrap";
+import {BrowserModule} from "@angular/platform-browser"
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic"
+import {AppComponent}  from "./app.component"
+import {RouterModule, PreloadAllModules} from "@angular/router";
 
-import { ROUTES } from "./app.routes";
+import {ROUTES} from "./app.routes";
 
 import {HomeComponent} from "./home"
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(ROUTES, { useHash: true, preloadingStrategy: PreloadAllModules })
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        NgbModule.forRoot(),
+        RouterModule.forRoot(ROUTES, {useHash: true, preloadingStrategy: PreloadAllModules})
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
