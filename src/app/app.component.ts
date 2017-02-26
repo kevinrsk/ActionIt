879066e45ae21ee38fc10ef2a7931edb1a917c82
Kevin Jones <kevin@rocksolidknowledge.com>
2017-02-26 08:55:26 +0000

    Minor code layout changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import '../../public/css/styles.scss';
 import {ToDoService} from './services/ToDoService';
-import {ProjectService} from "./services/ProjectService";
+import {ProjectService} from './services/ProjectService';
 
 @Component({
-  selector: 'app',
-  templateUrl: './app.component.html',
-  styles: [require('./app.component.scss').toString()],
+    selector: 'app',
+    templateUrl: './app.component.html',
+    styles: [require('./app.component.scss').toString()],
     providers: [ToDoService, ProjectService]
 })
-export class AppComponent { }
+export class AppComponent {
+}
